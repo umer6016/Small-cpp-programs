8c7fbff9b51ebc05f5a7e0f09a1bee612a4e15b0
Muhammad Umer Farooq <umer6016@hotmail.com>
2024-04-14 23:39:46 +0500

    Add files via upload

---
+++ Integer equivalent of a character.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main(){
+/*2.27 (Integer Equivalent of a Character) Here is a peek
+ahead. In this chapter you learned about integers and the type
+int . C++ can also represent uppercase letters, lowercase
+letters and a considerable variety of special symbols. C++ uses
+small integers internally to represent each different character.
+The set of characters a computer uses and the corresponding
+integer representations for those characters are called that
+computer’s character set. You can print a character by
+enclosing that character in single quotes, as with
+cout << 'A'; // print an uppercase A
+You can print the integer equivalent of a character using
+static_cast as follows:
+cout << static_cast<int>('A'); // print 'A' as an integer
+This is called a cast operation (we formally introduce casts in
+Chapter 4). When the preceding statement executes, it prints
+the value 65 (on systems that use the ASCII character set).
+Write a program that prints the integer equivalent of a character
+typed at the keyboard. Store the input in a variable of type
+char . Test your program several times using uppercase letters,
+lowercase letters, digits and special characters (such as $ ).*/
+
+//Declaring a char type variable
+char input;
+
+//Prompting the user to enter a character
+std::cout<<"Enter a character "<<std::endl;
+std::cin>>input;
+
+//static casting the character taken from the user and printing it
+std::cout<<"The character set integer value corresponding to the character is "<<static_cast<int>(input); 
+}
